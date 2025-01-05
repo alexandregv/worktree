8398c1c728462a0f8eba89006c655333ef0dce83
Alexandre GV. <contact@alexandregv.fr>
2025-01-05 02:08:21 +0100

    refactor: move some code from main to git.go

--- git.go
+++ git.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"text/tabwriter"
 )
 
 // Worktree represents a Git worktree with its attributes.
@@ -15,6 +16,39 @@ type Worktree struct {
 	Bare     bool
 	Detached bool
 	Locked   bool
+}
+
+func BuildTabulatedList(worktrees []*Worktree) (list []string) {
+	// Capture the tabbed output in a buffer
+	var buf bytes.Buffer
+	writer := tabwriter.NewWriter(&buf, 0, 0, 2, ' ', 0)
+
+	// Loop through worktrees and write their formatted output
+	for i, wt := range worktrees {
+		var str string
+		if wt.Bare {
+			str = fmt.Sprintf("%d: %s (bare)", i, wt.Path)
+		} else {
+			str = fmt.Sprintf("%d: %s\t[%s]\t%s", i, wt.Path, wt.Head[:7], wt.Branch)
+		}
+		if wt.Locked {
+			str += "\tlocked"
+		}
+		fmt.Fprintln(writer, str)
+	}
+
+	// Flush the writer to write the data into the buffer
+	writer.Flush()
+
+	// Capture the tabbed content as an array of strings
+	lines := bytes.Split(buf.Bytes(), []byte("\n"))
+	for _, line := range lines {
+		if string(line) == "" {
+			continue
+		}
+		list = append(list, string(line))
+	}
+	return list
 }
 
 // ParseWorktrees parses the output of `git worktree list --porcelain -z` into Worktree structs.
