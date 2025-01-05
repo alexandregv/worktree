e68fd1bc65b8edfdfa46b7a68ef765187e43b8b0
Alexandre GV. <contact@alexandregv.fr>
2025-01-05 23:47:42 +0100

    feat(root-cmd): allow switching to a worktree, with dynamic completion

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/alexandregv/worktree/pkg/core"
+	"github.com/alexandregv/worktree/pkg/git"
 	"github.com/spf13/cobra"
 )
 
@@ -12,8 +15,40 @@ var rootCmd = &cobra.Command{
 	Use:   "worktree",
 	Short: "Git worktree CLI utility",
 	Long:  `CLI utility to easily navigate between Git worktrees, list them, clone a multiple-worktrees-enabled repo, etc.`,
+	Args:  cobra.MatchAll(cobra.RangeArgs(0, 1), cobra.OnlyValidArgs),
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) (validArgs []string, directive cobra.ShellCompDirective) {
+		worktrees, err := git.GitWorktreeList()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "worktree: Could not get Git worktrees: %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		for _, wt := range worktrees {
+			split := strings.Split(wt.Path, "/")
+			validArgs = append(validArgs, split[len(split)-1])
+		}
+		return validArgs, directive
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		core.OpenTUI()
+		if len(args) == 0 {
+			core.OpenTUI()
+			return
+		}
+
+		worktrees, err := git.GitWorktreeList()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "worktree: Could not get Git worktrees: %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		for _, wt := range worktrees {
+			if strings.HasSuffix(wt.Path, "/"+args[0]) {
+				fmt.Println(wt.Path)
+				os.Exit(0)
+			}
+		}
+		fmt.Fprintf(os.Stderr, "worktree: Could not find Git worktree: %s\n", args[0])
+		os.Exit(1)
 	},
 }
 
