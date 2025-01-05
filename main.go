9a8c622082e7c31fa7a26f7074292b2ede72d9a8
Alexandre GV. <contact@alexandregv.fr>
2025-01-05 15:01:09 +0100

    feat(fzf): add catppuccin macchiato theme

--- main.go
+++ main.go
@@ -28,6 +28,10 @@ func main() {
 			"--jump-labels=0123456789;:,<.>/?'\"!@#$%^&*",
 			"--preview=sh -c \"git -C {3} log --color=always --oneline --graph --decorate --all -n20\"",
 			"--preview-window=right,45%",
+			"--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796",
+			"--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6",
+			"--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796",
+			"--color=selected-bg:#494d64",
 		},
 	)
 	if err != nil {
