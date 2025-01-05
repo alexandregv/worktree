28b85201fc7ee24edd7d1a3e8b275d07dd9eef94
Alexandre GV. <contact@alexandregv.fr>
2025-01-05 15:05:13 +0100

    refactor(fzf-options): move init func in fzf.go

--- fzf.go
+++ fzf.go
@@ -2,7 +2,34 @@ package main
 
 import fzf "github.com/junegunn/fzf/src"
 
-func initFzfOptions(inputs []string, customOptions []string) (options *fzf.Options, err error) {
+func InitFzfOptions(inputs []string) (options *fzf.Options, err error) {
+	options, err = fromDefaults(
+		inputs,
+		[]string{
+			"--height=40%",
+			"--prompt=worktree: ",
+			"--with-nth=2..",
+
+			// Jump to a line with Space + <n>
+			"--bind=space:jump",
+			"--jump-labels=0123456789;:,<.>/?'\"!@#$%^&*",
+
+			// Preview window with git log on the right
+			// sh -c '' is used to expand $HOME
+			"--preview=sh -c \"git -C {3} log --color=always --oneline --graph --decorate --all -n20\"",
+			"--preview-window=right,45%",
+
+			// Theme: Catppuccin Macchiato (https://github.com/catppuccin/fzf)
+			"--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796",
+			"--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6",
+			"--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796",
+			"--color=selected-bg:#494d64",
+		},
+	)
+	return options, err
+}
+
+func fromDefaults(inputs []string, customOptions []string) (options *fzf.Options, err error) {
 	options, err = fzf.ParseOptions(true, customOptions)
 	if err != nil {
 		return nil, err
--- main.go
+++ main.go
@@ -18,29 +18,7 @@ func main() {
 	}
 
 	// Init fzf options with defaults + our custom values
-	fzfOptions, err := initFzfOptions(
-		BuildWorktreeList(worktrees),
-		[]string{
-			"--height=40%",
-			"--prompt=worktree: ",
-			"--with-nth=2..",
-
-			// Jump to a line with Space + <n>
-			"--bind=space:jump",
-			"--jump-labels=0123456789;:,<.>/?'\"!@#$%^&*",
-
-			// Preview window with git log on the right
-			// sh -c '' is used to expand $HOME
-			"--preview=sh -c \"git -C {3} log --color=always --oneline --graph --decorate --all -n20\"",
-			"--preview-window=right,45%",
-
-			// Theme: Catppuccin Macchiato (https://github.com/catppuccin/fzf)
-			"--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796",
-			"--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6",
-			"--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796",
-			"--color=selected-bg:#494d64",
-		},
-	)
+	fzfOptions, err := InitFzfOptions(BuildWorktreeList(worktrees))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "worktree: Could not initalize fzf options: %s\n", err.Error())
 		os.Exit(fzf.ExitError)
