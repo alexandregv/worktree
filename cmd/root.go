9ed01ffd1c975c2bfbfff36ae48cb25b54063a2b
Alexandre GV. <contact@alexandregv.fr>
2025-01-05 23:05:58 +0100

    fix(root-cmd): remove invalid args completion (was a test)

--- cmd/root.go
+++ cmd/root.go
@@ -12,12 +12,6 @@ var rootCmd = &cobra.Command{
 	Use:   "worktree",
 	Short: "Git worktree CLI utility",
 	Long:  `CLI utility to easily navigate between Git worktrees, list them, clone a multiple-worktrees-enabled repo, etc.`,
-	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-		if len(args) != 0 {
-			return nil, cobra.ShellCompDirectiveNoFileComp
-		}
-		return []string{"dev", "main"}, cobra.ShellCompDirectiveNoFileComp
-	},
 	Run: func(cmd *cobra.Command, args []string) {
 		core.OpenTUI()
 	},
