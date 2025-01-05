7a02b91ca4688b43c158a5b6b44350ec48ae6013
Alexandre GV. <contact@alexandregv.fr>
2025-01-06 00:09:05 +0100

    fix(root-cmd): fix args count, let cobra handle it

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,6 @@ var rootCmd = &cobra.Command{
 	Use:   "worktree",
 	Short: "Git worktree CLI utility",
 	Long:  `CLI utility to easily navigate between Git worktrees, list them, clone a multiple-worktrees-enabled repo, etc.`,
-	Args:  cobra.MatchAll(cobra.RangeArgs(0, 1)),
 	Run: func(cmd *cobra.Command, args []string) {
 		core.OpenTUI()
 	},
